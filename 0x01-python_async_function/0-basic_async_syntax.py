b40170bd084ffa1635a4090eaeabae71d93748f1
Natnael-Negash <nnegash658@gmail.com>
2024-08-19 08:08:00 -0900

    basics of async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
+"""
+module containing a courtine that introduces a 
+rondom delay and returns it.
+"""
 import asyncio
 import random
+
 
 async def wait_random(max_delay: int = 10) -> float:
     """
